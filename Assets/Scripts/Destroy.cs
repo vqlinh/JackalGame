1cc3834946aa8c82317cd77513a2bb08e09cedaa
vqlinh <vuongquanglinh1234@gmail.com>
2024-01-12 11:07:23 +0700

    +

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -1,16 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
-    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Grenade")
-        {
-            this.gameObject.SetActive(false);
-
-        }
+        if (collision.gameObject.tag == "Grenade") this.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
@@ -16,57 +13,53 @@ public class Ship : MonoBehaviour
     public bool IsMoving;
     public float AttackDistance;
     private Transform TargetPosition;
-    // Start is called before the first frame update
+
+    int countBullet = 0;
+    int countGrenade = 0;
+
+
     void Start()
     {
         TargetPosition = GameObject.FindGameObjectWithTag("Player").transform;
         IsMoving = true;
     }
-    
-    
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Grenade")|| collision.gameObject.CompareTag("Bullet"))
+        if (collision.gameObject.CompareTag("Grenade"))
         {
-            this.gameObject.SetActive(false);
+            countGrenade++;
+            if (countGrenade == 2) Destroy(this.gameObject);
+        }
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            countBullet++;
+            if (countBullet == 6) Destroy(this.gameObject);
         }
     }
 
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, TargetPosition.position); // vi tri cua nhan vat va crep
+        float distance = Vector3.Distance(transform.position, TargetPosition.position);
         if (distance <= AttackDistance) // check if distance is less than or equal to AttackDistance
         {
             if (transform.localPosition.y > DownPos) // Nếu chưa đi hết xuống dưới
             {
                 transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, DownPos), Speed * Time.deltaTime);
-
-                if (transform.localPosition.y <= DownPos)
-                {
-                    MovingBot = true;
-                }
+                if (transform.localPosition.y <= DownPos) MovingBot = true;
             }
-
-
-
             if (Time.time > nextTimeToFire)
             {
                 nextTimeToFire = Time.time + 1 / FireRate;
                 shoot();
             }
-           
         }
 
-
-      
         void shoot()
         {
             GameObject BulletIns = Instantiate(Bullet, ShootPos.position, Quaternion.identity);
             Vector2 direction = (TargetPosition.position - ShootPos.position).normalized;
             BulletIns.GetComponent<Rigidbody2D>().AddForce(direction * Force);
-            // Gọi đệ quy
         }
     }
-
 }
